9ecea4815cdca923b8b0762f66ae7f12fbf8c207
jiyoung <jypark912@naver.com>
2023-09-20 10:50:49 +0900

    app 수정

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { BrowserRouter , Route, Routes } from "react-router-dom";
+import {  Route, Routes } from "react-router-dom";
 import 'bootstrap/dist/css/bootstrap.min.css';
 import HomeView from "./views/HomeView";
 import "./assets/scss/style.scss"
@@ -9,11 +9,9 @@ const App = () => {
 
     return (
     <div className="app__banner app">
-            <BrowserRouter>
-                <Routes>
-                    <Route path="/" element={<HomeView />} />
-                </Routes>
-            </BrowserRouter>
+        <Routes>
+            <Route path="/" element={<HomeView />} />
+        </Routes>
     </div>
     );
 };
